b5d2403fad1fe14eb3b85f9dc3c7ce3f73027494
Saketh7903 <161412903+Saketh7903@users.noreply.github.com>
2024-07-12 10:31:51 +0530

    Add files via upload

---
+++ reversenumber.java
@@ -0,0 +1,15 @@
+Class reversenumber
+{
+public static void main(String args[0])
+{
+int n=Integer.parseInt();
+int rev=0;
+int r;
+while(1){
+int r=n % 10;
+rev=(rev * 10)+r;
+n=n/10;
+}
+System.out.println("rev);
+}
+}
